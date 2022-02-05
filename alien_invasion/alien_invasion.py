e41d69d0b686c15efc635f539da49f4c597377e0
tessa <64588494+carly-code@users.noreply.github.com>
2022-02-05 18:44:08 +0800

    add new thing

---
+++ alien_invasion/alien_invasion.py
@@ -0,0 +1,45 @@
+
+import pygame 
+from pygame.sprite import Group  
+from settings import Settings 
+from ship import Ship
+import game_functions 
+from alien import Alien 
+from game_stats import GameStats
+from button import Button 
+from scoreboard import Scoreboard
+def run_game():
+	pygame.init() #初始化pygame 
+	ai_settings=Settings()  #一个Settings实例
+	screen=pygame.display.set_mode((ai_settings.screen_width, #生成一个屏幕surface
+		ai_settings.screen_height))	 #一个元组 屏幕长宽 
+	pygame.display.set_caption('Alien Invasion')  #设置游戏名字 
+	play_button=Button(ai_settings,screen,'play')
+	stats=GameStats(ai_settings)
+	ship=Ship(ai_settings,screen) #传给Ship类 一个屏幕 生成ship实例
+	bullets=Group() #生成bullets实例 
+	scoreboard=Scoreboard(ai_settings,screen,stats)
+	aliens=Group()
+	game_functions.creat_fleet(ai_settings,screen,ship,aliens)
+	while True:	
+		game_functions.check_events(ai_settings,screen,stats,play_button,ship,bullets,aliens) #不断的调用check_events方法 
+		game_functions.hide_mouse(stats)
+		if stats.game_active:
+			
+			ship.update()#调用ship的update方法 
+			
+			game_functions.update_bullets(ai_settings,screen,stats,scoreboard,ship,aliens,bullets)
+			game_functions.update_aliens(ai_settings,stats,screen,ship,aliens,bullets)
+		
+		
+		game_functions.update_screen(ai_settings,screen,stats,scoreboard,ship,bullets,aliens,play_button) #调用更新屏幕（check_update)的方法 
+	
+
+run_game()
+
+
+# 游戏过程 1.不断地运行 check_events,对ship和bullets的标志进行修改  
+#		  2.不断的根据自身的标志 改变自身的位置 属性 等
+#         3.根据ship和bullets的位置 刷新屏幕 
+
+		 
---
+++ alien_invasion/ship.py
@@ -0,0 +1,29 @@
+import pygame 
+class Ship():
+	def __init__(self,ai_settings,screen):
+		self.ai_settings=ai_settings #ai_settings属性是一个Settings实例 
+		#load the image and get the around rectangle 
+		self.screen = screen #screen属性是一个 
+		self.image=pygame.image.load('images/ship.bmp')  #传给ship类 一个图片 
+		self.rect=self.image.get_rect() #ship类的实例的矩形等于 那个图片取矩形 
+		self.screen_rect=screen.get_rect() #ship类里面的屏幕矩形 等于屏幕取矩形 这个用于之后设置实例的位置
+		# put ship at the bottom of screen 
+		self.rect.centerx=self.screen_rect.centerx #设置实例的位置 实例的x中心等于获取的屏幕的x中心
+		self.rect.bottom=self.screen_rect.bottom #设置实例的位置 实例的底部等于获取的屏幕的底部
+		self.center=float(self.rect.centerx) #获取实例的x中心 ，并且转为浮点数 之后改变self.center，根据self.center改变实例位置
+		self.moving_right=False #设置两个标志 
+		self.moving_left=False
+	def update(self):
+		'''更新飞船位置  虽然最后会把self.center取成整数,但是self.center的变化是通过小数变化的,不会有太大的误差'''
+		if self.moving_right and self.screen_rect.right> self.rect.right :
+			self.center+= self.ai_settings.ship_speed_factor 
+		elif self.moving_left and self.rect.left> self.screen_rect.left :
+			self.center+= -(self.ai_settings.ship_speed_factor )
+		self.rect.centerx=self.center #根据self.center改变实例位置
+
+
+	def blitme(self):
+
+		self.screen.blit(self.image,self.rect) #将得到的屏幕 显示实例  两个参数 一个图像 一个位置 
+	def center_ship(self):
+		self.center=self.screen_rect.centerx 
